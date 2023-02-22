eb19ff56255f598e85d595104d43a17af3b1e569
btruo <brandon.truong1997@gmail.com>
2023-02-21 19:20:14 -0500

    Feb 21 2023 update

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # FCC: Random Quote Machine
 
-This is a random quote generator made as part of the freeCodeCamp Front End Development Libraries Certification Curriculum. I used ReactJS, Bootstrap styling, and a JSON API to build and implement the quote generator. There is a button to share the quote onto twitter.
+This is a random quote generator made as part of the freeCodeCamp Front End Development Libraries Certification Curriculum.
 
-It is also uploaded online at: https://codepen.io/btruo/pen/VwrppRK
+I used ReactJS, CSS and Bootstrap styling, and a JSON API to build and implement the quote generator. There is a button to share the quote onto twitter.
+
+It is also uploaded online at: https://codepen.io/btruo/pen/gOdaxGK
--- src/App.js
+++ src/App.js
@@ -2,96 +2,88 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faTwitter} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 
-const API = 
-"https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
+const API =
+  "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
 
-
+// main App
 function App() {
   return (
-    <div className='wrapper'>
-      <div id='quote-box' className="d-flex justify-content-center align-items-center">
-        <QuoteGen />
+    <div className='wrapper' id='quote-box'>
+      <h1 className='title'>Random Quote Generator</h1>
+      <QuoteGen />
+      <div className="footer">
+        By Brandon Truong
       </div>
-        <div className="footer">By Brandon Truong</div>
     </div>
   );
 };
 
+// Component QuoteGen consisting of constructor, API fetch, RNG generator, and render to the DOM.
 class QuoteGen extends React.Component {
-  constructor(props){
-  super (props)
+  constructor(props) {
+    super(props)
     this.state = {
-      quotes:[
+      quotes: [
         {
-        quote:"Life isnt about getting and having, its about giving and being.",
-        author:"Kevin Kruse"
+          quote: "Life isnt about getting and having, its about giving and being.",
+          author: "Kevin Kruse"
         }
-    ],
+      ],
       index: 0,
       error: null,
     }
     this.getRandomIndex = this.getRandomIndex.bind(this);
     this.componentDidMount = this.componentDidMount.bind(this);
   }
-  
-    componentDidMount() {
-      fetch (API)
-        .then (res => res.json())
-        .then(
-          (res) => {  
-            this.setState({
-              quotes: res.quotes
-            },this.getRandomIndex);
-          });
-      }
-      getRandomIndex =() => {
-        const { quotes } = this.state;
-        const ranIndex = Math.floor(Math.random() * quotes.length)
-        this.setState({
-          index: ranIndex 
-        })
-      };
-  
-
-
-
+  // API fetch
+  componentDidMount() {
+    fetch(API)
+      .then(res => res.json())
+      .then(
+        (res) => {
+          this.setState({
+            quotes: res.quotes
+          }, this.getRandomIndex);
+        });
+  }
+  // RNG for next quote
+  getRandomIndex = () => {
+    const { quotes } = this.state;
+    const ranIndex = Math.floor(Math.random() * quotes.length)
+    this.setState({
+      index: ranIndex
+    })
+  };
+  // render to the DOM
   render() {
     const { quotes, index } = this.state;
     const ourQuote = quotes[index];
     const tweetURL = `https://twitter.com/intent/tweet/?text=${ourQuote.quote} - ${ourQuote.author}`;
 
-      return (       
-      <div className="row gx-2">
-    <div className="col box p-5 rounded" id="boxed">
-    { 
-      ourQuote && (
-        <div id='text'>
-          <p>"{ourQuote.quote}"</p>
+    return (
+      <main className='content'>
+        {
+          ourQuote && (
+            <p id='text'>"{ourQuote.quote}"</p>
+          )
+        }
+        {
+          ourQuote && (
+            <cite id='author'>- {ourQuote.author}</cite>
+          )
+        }
+        <div className='buttons'>
+          <a className="btn btn-primary twitter btn-lg" id='tweet-quote' target="_blank" href={tweetURL}>
+            <FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon>
+          </a>
+          <button className="btn btn-primary btn-lg" id="new-quote" onClick={this.getRandomIndex}>New Quote</button>
         </div>
-      )
-    }
-    {
-      ourQuote && (
-        <div id="author">
-          <cite>- {ourQuote.author}</cite> 
-        </div> 
-        )
-      }
-      <div className="d-flex justify-content-between buttons">
-
-              <a className="btn btn-primary twitter" target="_blank" href={tweetURL}>
-              <FontAwesomeIcon icon= {faTwitter}></FontAwesomeIcon> 
-              </a>
-              <button className = "btn btn-primary" onClick={this.getRandomIndex}>New Quote</button>
-            </div>
-
-          </div>
-          </div>
-      )
-      }
+      </main>
+    )
   }
+}
 
 export default App;
